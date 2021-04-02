8269c7f2206f23c123e00e0b1772f30d7908ea32
Andy Vu <andyavu@gmail.com>
2021-04-02 11:41:49 -0700

    create files

---
+++ src/cs4310hw1/CS4310Hw1.java
@@ -0,0 +1,17 @@
+//******************************************************************************
+// Author      : Andy Vu
+// Project     : Simulating Job Scheduler and Performance Analysis
+// Course      : CS 4310
+// File        : CS4310Hw1.java
+// Description : 
+//******************************************************************************
+
+package cs4310hw1;
+
+public class CS4310Hw1 
+{
+    public static void main(String args[]) 
+    {
+        
+    }
+}
---
+++ src/cs4310hw1/FCFS.java
@@ -0,0 +1,14 @@
+//******************************************************************************
+// Author      : Andy Vu
+// Project     : 
+// Course      : 
+// File        : FCFS.java
+// Description : First-Come-FIrst-Serve (FCFS)
+//******************************************************************************
+
+package cs4310hw1;
+
+public class FCFS 
+{
+    
+}
